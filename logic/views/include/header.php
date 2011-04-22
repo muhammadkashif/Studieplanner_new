5eaadf01ebcb21b56c6329700761c41101cb2e2a
Joris Ooms <joris@frenchyimfaking.com>
2011-04-22 15:04:02 +0200

    started login view

---
+++ logic/views/include/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="nl" xml:lang="nl">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="title" content="Studieplanner.be | Plannen op je best!" />
+		<meta name="description" content="Studieplanner.be is een applicatie om je leerstof én vrije tijd grondig te plannen!" />
+		<meta name="keywords" content="studeren, student, studie, planning, planner, kalender, leerstof, leerkracht, middelbaar, vrije, tijd, leren, werken, diploma" />
+
+		<title>Studieplanner.be</title>
+
+		<!--[if IE]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	</head>
+	<body>
+
+
