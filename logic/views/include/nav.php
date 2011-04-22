83f6f6121ead9d7a787281e9064029c8571c3f45
Joris Ooms <joris@frenchyimfaking.com>
2011-04-23 00:35:41 +0200

    include/nav.php: menu include login.css: update to login form css global.css: removed some unnecessary code nav.css: menu css

---
+++ logic/views/include/nav.php
@@ -0,0 +1,34 @@
+<header>
+	<div id="yellow"></div>
+	<div id="container_h">
+		<a href="/site">
+			<?php
+			
+				$img_logo = array(
+								'src'	=>		'assets/images/header_logo.png',
+								'alt'	=>		'Het logo van Studieplanner.be',
+								'title'	=>		'Plannen op je best!',
+								'id'	=>		'logo'
+							);
+				echo img($img_logo);
+				
+			?>
+		</a>
+		<nav>
+			<ul>
+				<li <? if($page_title == "Student") {?>class="active"<? } ?>>
+					<a href="<?= $base_url; ?>student/">Home</a>
+				</li>
+				<li <? if($page_title == "Planner") {?>class="active"<? } ?>>
+					<a href="<?= $base_url; ?>planner/">Planner</a>
+				</li>
+				<li <? if($page_title == "Informatie") {?>class="active"<? } ?>>
+					<a href="<?= $base_url; ?>student/informatie">Informatie</a>
+				</li>
+				<li <? if($page_title == "Berichten") {?>class="active"<? } ?>>
+					<a href="<?= $base_url; ?>student/berichten">Berichten</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+</header>
