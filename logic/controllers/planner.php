e0493bc645ce0d4f7e3c5eab4780e29121be61a3
Joris Ooms <joris@frenchyimfaking.com>
2011-04-23 23:13:36 +0200

    renamed planner/days.php to planner/index.php + controller fixup.

--- logic/controllers/planner.php
+++ logic/controllers/planner.php
@@ -7,26 +7,34 @@ class Planner extends MY_Controller
 		
 		parent::__construct();
 		$this->load->model('planner_model');
-		
-		
+
 	}
 	
-	public function index($month = '', $year = '')
+	public function index()
 	{
+
 		$init = $this->init->set();
 		
 		$this->load->view('include/header', $init);
 		$this->load->view('include/nav');
 		
-		if( ! empty($month) && ! empty($year))
-		{
-			$dates = $this->planner_model->create_date_list($month, $year);
-		}
-		else
-		{
-			$dates = $this->planner_model->create_date_list();
-		}
-		$this->load->view('planner/days', $dates);
+
+		$dates = $this->planner_model->create_date_list();
+
+		$this->load->view('planner/index', $dates);
+		
+		$this->load->view('include/footer');
+
 	}
 
+	public function change_dates()
+	{
+
+		$month = $this->input->post('month');
+		$year = $this->input->post('year');
+		$dates = $this->planner_model->create_date_list($month, $year);
+		
+	    $this->load->view('planner/index', $dates);
+
+	}
 }
---
+++ logic/views/planner/index.php
@@ -0,0 +1,87 @@
+<div id="content">
+	<script type="text/javascript">
+	
+		$(document).ready(function()	{
+
+			$("#day_list").fadeIn(600);
+
+			// init
+			var current_month = <?= $init['current_month']; ?>;
+			var current_year = <?= $init['current_year'] ?>;
+			
+			// previous, next month ajax call
+			$(".previous, .next").click(function(event)	{
+				$("#days_loader").show();
+				if(event.target.id == "previous")
+				{
+					if(current_month == 1)
+					{
+						current_month = 12;
+						current_year--;
+					}
+					else
+					{
+						current_month--;				
+					}
+				}
+				if(event.target.id == "next")
+				{
+					if(current_month == 12)
+					{
+						current_month = 1;
+						current_year++;
+					}
+					else
+					{
+						current_month++;
+					}
+				}
+				
+				$.ajax({
+					type: "POST",
+					url: "/planner/change_dates",
+					data: { month: current_month, year: current_year },
+					success: function(data)
+					{
+						$("#content").html(data);
+					}
+				});
+			});
+		});
+
+	</script>
+	<div id="overview_days">
+		<p class="month"><?= ucfirst($init['curr_month_name']) . " " . $init['current_year']; ?></p>
+		
+		<img src="http://www.obscura-design.be/images/days_loader.gif" id="days_loader"/> 
+		
+		<div class="clearfix"></div> 
+		
+		<div class="month_selector previous"><img src="http://www.obscura-design.be/images/arrow_l.png" id="previous" /></div>
+		
+		<ul id="day_list" class="<?php
+				switch($init['days_in_curr_month'])
+				{
+					case 28:
+						echo 'twentyeight';
+						break;
+					case 29:
+						echo 'twentynine';
+						break;
+					case 30:
+						echo 'thirty';
+						break;
+					case 31:
+						echo 'thirtyone';
+						break;
+				}
+				?>">
+			<?php foreach($dates as $day => $date): ?>
+				<li class="<?php echo $date['type']; if($date['event_count'] > 0){ echo " has_event";}?>" value="<?= $day ?>"><?= $day; ?></li>
+			<?php endforeach; ?>
+		</ul>
+		
+		<div class="month_selector next"><img src="http://www.obscura-design.be/images/arrow_r.png" id="next" /></div> 
+		
+	</div>
+</div>
